9c9b50aa07938abd492b61846d83d3037b160a7a
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-25 21:17:11 +0530

    defined create function which is creatinf question in questionsCOntroller

--- controllers/questionsController.js
+++ controllers/questionsController.js
@@ -5,7 +5,26 @@ const Option = require('../models/optionSchema');
 
 //questions
 module.exports.questionCreate = function(req, res){
+    try {
+        // getting the questions from request body
+        let questions = req.body.questions;
+        console.log(questions);
 
+        // create a question in db
+        for(let i = 0; i < questions.length; i++){
+            Question.create({
+                title: questions[i].title
+            });
+        }
+
+        return res.status(200).json({
+            messgae: "Question's is created successfully"
+        });
+    } catch (err) {
+        return res.status(500).json({
+            messgae: err.messgae || "Some error while creating question"
+        });
+    }
 }
 
 // add options to specific questions
