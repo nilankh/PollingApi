c637d361178d0f0b18ecc0df33085c95a318f754
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-25 20:30:33 +0530

    created optionsController just defined controller yt to be write functiins within it

---
+++ controllers/optionsController.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Question = require('../models/question');
+const Option = require('../models/optionSchema');
+
+
+module.exports.delete = function(req, res){
+
+};
+
+module.exports.voteAdd = function(req, res){
+
+};
