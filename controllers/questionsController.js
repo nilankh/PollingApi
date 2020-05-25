63a0cd3eea679c8f9752bff291b7a909fb64c510
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-25 20:37:03 +0530

    created questionCOntroller in controller file just define the all the name yet to be wriiten functiins within it

---
+++ controllers/questionsController.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Question = require('../models/question');
+const Option = require('../models/optionSchema');
+
+
+//questions
+module.exports.questionCreate = function(req, res){
+
+}
+
+// add options to specific questions
+module.exports.addOptions = function(req, res){
+
+}
+
+// To view a question
+module.exports.view = function(req, res){
+
+}
+
+// delete questions
+module.exports.deleteQuestion = function(req, res){
+
+}
